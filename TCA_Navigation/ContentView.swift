468eea9dc25c4234a84cb1f02b8e0ee41c079a68
sauna1137 <subscription.search@gmail.com>
2024-09-03 07:34:41 +0900

    Initial Commit

---
+++ TCA_Navigation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCA_Navigation
+//
+//  Created by KS on 2024/09/03.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCA_Navigation/TCA_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCA_NavigationApp.swift
+//  TCA_Navigation
+//
+//  Created by KS on 2024/09/03.
+//
+
+import SwiftUI
+
+@main
+struct TCA_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
